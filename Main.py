7c3b2f41115b3d097ca4a6845f84d1c3654397e3
netzerodash <=>
2016-10-31 01:55:06 +0700

    filter data

--- Main.py
+++ Main.py
@@ -1,8 +1,15 @@
 import numpy
 import matplotlib
 import pandas as pd
-import Quandl
+import quandl as q
 
-data = Quandl.get("WIKI/GOOGL")
+data = q.get("WIKI/GOOGL")
+
+#print(data.head())
+
+data = data[['Adj. Open','Adj. High','Adj. Low','Adj. Close','Adj. Volume']]
+data['HL_PCT'] = (data['Adj. High'] - data['Adj. Close']) / data['Adj. Close'] * 100.0
+data['PCT_change'] = (data['Adj. Close'] - data['Adj. Open']) / data['Adj. Open'] * 100.0
+data = data[['Adj. Close','HL_PCT','PCT_change','Adj. Volume']]
 
 print(data.head())
